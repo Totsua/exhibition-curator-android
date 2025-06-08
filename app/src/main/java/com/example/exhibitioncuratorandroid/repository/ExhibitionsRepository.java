b64eaf9e249e8380b4e7a13153e0f033122de01b
Shamar <Shamar098@gmail.com>
2025-06-08 20:36:13 +0100

    feat: add exhibition view model and repository

---
+++ app/src/main/java/com/example/exhibitioncuratorandroid/repository/ExhibitionsRepository.java
@@ -0,0 +1,55 @@
+package com.example.exhibitioncuratorandroid.repository;
+
+import android.app.Application;
+import android.widget.Toast;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.exhibitioncuratorandroid.model.Exhibition;
+import com.example.exhibitioncuratorandroid.model.ExhibitionCreateDTO;
+import com.example.exhibitioncuratorandroid.service.CuratorAPIService;
+import com.example.exhibitioncuratorandroid.service.RetroFitInstance;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ExhibitionsRepository {
+    private MutableLiveData<List<Exhibition>> liveExhibitionList = new MutableLiveData<>();
+    private Application application;
+
+    public ExhibitionsRepository(Application application){this.application = application;}
+
+    public void createExhibition(String title, MutableLiveData<Boolean> isLoading){
+        CuratorAPIService curatorAPIService = RetroFitInstance.getService();
+        Call<Void> call = curatorAPIService.createExhibition(new ExhibitionCreateDTO(title));
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                isLoading.setValue(false);
+                switch (response.code()){
+                    case 201:
+                        Toast.makeText(application, "Exhibition Created", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 404:
+                        Toast.makeText(application,"Title Already Used",Toast.LENGTH_SHORT).show();
+                    case 500:
+                        Toast.makeText(application, "Internal Server Error", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        Toast.makeText(application, "Unknown Server Error: Contact the developer", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                isLoading.setValue(false);
+                Toast.makeText(application, "Network Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/exhibitioncuratorandroid/viewmodel/ExhibitionsViewModel.java
@@ -0,0 +1,27 @@
+package com.example.exhibitioncuratorandroid.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.exhibitioncuratorandroid.model.Exhibition;
+import com.example.exhibitioncuratorandroid.repository.ExhibitionsRepository;
+
+import java.util.List;
+
+public class ExhibitionsViewModel extends AndroidViewModel {
+    private MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
+    private ExhibitionsRepository exhibitionsRepository;
+
+    public ExhibitionsViewModel(@NonNull Application application) {
+        super(application);
+        this.exhibitionsRepository = new ExhibitionsRepository(application);
+    }
+    public void createExhibition(String title){
+        exhibitionsRepository.createExhibition(title,isLoading);
+    }
+    public LiveData<Boolean> getIsLoading(){return getIsLoading();}
+}
