2a052320ee37a0d4e08eb4d87d1eca1a62db22ae
Shamar <Shamar098@gmail.com>
2025-06-08 09:39:41 +0100

    feat: artist and artwork implement Parcelable

--- app/src/main/java/com/example/exhibitioncuratorandroid/model/Artist.java
+++ app/src/main/java/com/example/exhibitioncuratorandroid/model/Artist.java
@@ -1,6 +1,9 @@
 package com.example.exhibitioncuratorandroid.model;
 
-public class Artist {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Artist implements Parcelable {
     private Long apiID;
     private String name;
 
@@ -9,6 +12,43 @@ public class Artist {
         this.apiID = apiID;
         this.name = name;
     }
+
+    protected Artist(Parcel in) {
+        if (in.readByte() == 0) {
+            apiID = null;
+        } else {
+            apiID = in.readLong();
+        }
+        name = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        if (apiID == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeLong(apiID);
+        }
+        dest.writeString(name);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Artist> CREATOR = new Creator<Artist>() {
+        @Override
+        public Artist createFromParcel(Parcel in) {
+            return new Artist(in);
+        }
+
+        @Override
+        public Artist[] newArray(int size) {
+            return new Artist[size];
+        }
+    };
 
     public String getName() {
         return name;
--- app/src/main/java/com/example/exhibitioncuratorandroid/model/Artwork.java
+++ app/src/main/java/com/example/exhibitioncuratorandroid/model/Artwork.java
@@ -1,6 +1,11 @@
 package com.example.exhibitioncuratorandroid.model;
 
-public class Artwork {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Artwork implements Parcelable {
 
     // todo: change 'id' to 'apiId'
     private Long id;
@@ -20,6 +25,32 @@ public class Artwork {
         this.imageUrl = imageUrl;
         this.artist = artist;
     }
+
+    protected Artwork(Parcel in) {
+        if (in.readByte() == 0) {
+            id = null;
+        } else {
+            id = in.readLong();
+        }
+        title = in.readString();
+        description = in.readString();
+        altText = in.readString();
+        apiOrigin = in.readString();
+        imageUrl = in.readString();
+        artist = in.readParcelable(Artist.class.getClassLoader());
+    }
+
+    public static final Creator<Artwork> CREATOR = new Creator<Artwork>() {
+        @Override
+        public Artwork createFromParcel(Parcel in) {
+            return new Artwork(in);
+        }
+
+        @Override
+        public Artwork[] newArray(int size) {
+            return new Artwork[size];
+        }
+    };
 
     public Long getId() {
         return id;
@@ -47,5 +78,26 @@ public class Artwork {
 
     public Artist getArtist() {
         return artist;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel parcel, int i) {
+        if (id == null) {
+            parcel.writeByte((byte) 0);
+        } else {
+            parcel.writeByte((byte) 1);
+            parcel.writeLong(id);
+        }
+        parcel.writeString(title);
+        parcel.writeString(description);
+        parcel.writeString(altText);
+        parcel.writeString(apiOrigin);
+        parcel.writeString(imageUrl);
+        parcel.writeParcelable(artist, i);
     }
 }
